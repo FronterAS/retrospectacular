7237f54ab1dd7c397f82804ee804b769d3b5a0a7
Gavin King <gavin.king@fronter.com>
2013-12-05 10:29:47 +0100

    Added CORS for the frontend

--- service/app.js
+++ service/app.js
@@ -7,4 +7,23 @@ app.get('/', function (req, res) {
     res.json('boom');
 });
 
+app.use(express.logger('dev'));
+
+var allowCrossDomain = function (req, res, next) {
+    if (!req.get('Origin')) {
+        return next();
+    }
+
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+    // res.set('Access-Control-Allow-Max-Age', 3600);
+
+    if ('OPTIONS' == req.method) {
+        return res.send(200);
+    }
+
+    next();
+}
+
 app.listen(3000);
