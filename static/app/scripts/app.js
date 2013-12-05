a18c1cdec4cc24e749eccc52af065b279d98afac
Gavin King <gavin.king@fronter.com>
2013-12-05 17:23:35 +0100

    Linting and 2 to 4 spaces

--- static/app/scripts/app.js
+++ static/app/scripts/app.js
@@ -1,24 +1,25 @@
 'use strict';
 
 angular.module('retrospectApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/choosetype', {
-        templateUrl: 'views/choosetype.html',
-        controller: 'ChoosetypeCtrl'
-      })
-      .when('/editpage/:noteType', {
-        templateUrl: 'views/editpage.html',
-        controller: 'EditpageCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }).config(['$httpProvider', function($httpProvider) {
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/choosetype', {
+                templateUrl: 'views/choosetype.html',
+                controller: 'ChoosetypeCtrl'
+            })
+            .when('/editpage/:noteType', {
+                templateUrl: 'views/editpage.html',
+                controller: 'EditpageCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+    }).config(['$httpProvider', function ($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
     }
--- static/app/scripts/services/tickets.js
+++ static/app/scripts/services/tickets.js
@@ -1,18 +1,20 @@
 'use strict';
 
 angular.module('retrospectApp')
-  .factory('tickets', ['$http', function ($http) {
+    .factory('tickets', ['$http', function ($http) {
+
     var tickets = {};
 
     tickets.getTickets = function (callback) {
         var url = 'http://petter.fronter.net:3000/retrospectives?callback=JSON_CALLBACK';
 
-        return $http.jsonp(url).success(function(data, status) {
-                callback(data);
-            }).error(function (data, status) {
-                // Stuff gone bad!
-                callback(data);
-            });
+        return $http.jsonp(url).success(function (data, status) {
+            callback(data);
+
+        }).error(function (data, status) {
+            // Stuff gone bad!
+            callback(data);
+        });
     };
 
     // Public API here
