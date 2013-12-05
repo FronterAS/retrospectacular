dcd666cacfaf642a2ab07994e329cd410b78f1f4
Gavin King <gavin.king@fronter.com>
2013-12-05 13:59:52 +0100

    Moving silly code to commented out area

--- service/handlers.js
+++ service/handlers.js
@@ -1,56 +1,5 @@
 var db = require('./wrapper');
 
-/*db.destroyIndex('retrospectives');*/
-db.checkIndexExists('retrospectives')
-    .then(function (exists) {
-        console.log('check index exists', exists);
-    })
-    .fail(function (err) {
-        console.log(err);
-    });
-
-db.checkIndexStatus('retrospectives')
-    .then(function (status) {
-        console.log('check index status ', status);
-    })
-    .fail(function (err) {
-        console.log(err);
-    });
-
-var teamData = {
-    name: 'Bogus team'
-};
-
-db.postData(teamData).ofType('team').into('retrospectives')
-    .then(function (result) {
-        console.log(result);
-    })
-    .fail(function (err) {
-        console.log(err);
-    });
-
-var retroData = {
-    'teamId': 102,
-    'type': 'pro',
-    'message': 'Learning new things'
-};
-
-db.postData(retroData).ofType('ticket').into('retrospectives')
-    .then(function (result) {
-        console.log(result);
-    })
-    .fail(function (err) {
-        console.log(err);
-    });
-
-db.getAll('ticket').from('retrospectives')
-    .then(function (result) {
-        console.log(result);
-    })
-    .fail(function (err) {
-        console.log(err);
-    });
-
 exports.getResults = function (req, res) {
     res.json('boom');
 };
--- service/wrapper.js
+++ service/wrapper.js
@@ -113,3 +113,60 @@ exports.destroyIndex = function (indexName) {
 
     return defer.promise;
 };
+
+
+// USAGE NOTES
+/*
+
+db.destroyIndex('retrospectives');
+
+db.checkIndexExists('retrospectives')
+    .then(function (exists) {
+        console.log('check index exists', exists);
+    })
+    .fail(function (err) {
+        console.log(err);
+    });
+
+db.checkIndexStatus('retrospectives')
+    .then(function (status) {
+        console.log('check index status ', status);
+    })
+    .fail(function (err) {
+        console.log(err);
+    });
+
+var teamData = {
+    name: 'Bogus team'
+};
+
+db.postData(teamData).ofType('team').into('retrospectives')
+    .then(function (result) {
+        console.log(result);
+    })
+    .fail(function (err) {
+        console.log(err);
+    });
+
+var retroData = {
+    'teamId': 102,
+    'type': 'pro',
+    'message': 'Learning new things'
+};
+
+db.postData(retroData).ofType('ticket').into('retrospectives')
+    .then(function (result) {
+        console.log(result);
+    })
+    .fail(function (err) {
+        console.log(err);
+    });
+
+db.getAll('ticket').from('retrospectives')
+    .then(function (result) {
+        console.log(result);
+    })
+    .fail(function (err) {
+        console.log(err);
+    });
+*/
