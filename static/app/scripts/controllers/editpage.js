a7aed29f8037e20a85ed6cbd1b38203d54694eef
Gavin King <gavin.king@fronter.com>
2013-12-06 12:51:21 +0100

    Added controls to the add ticket page

--- static/app/scripts/controllers/editpage.js
+++ static/app/scripts/controllers/editpage.js
@@ -2,26 +2,33 @@
 
 angular.module('retrospectApp')
 
-  .controller('EditpageCtrl', function ($scope, $routeParams, tickets) {
-    $scope.retroListElements = [];
+    .controller('EditpageCtrl', function ($scope, $routeParams, tickets) {
+        $scope.retroTickets = [];
+        $scope.role = 'pro';
 
-    $scope.noteType = $routeParams.noteType;
+        $scope.retroId = $routeParams.retroId;
 
-    $scope.retroId = $routeParams.retroId;
+        $scope.setRole = function (role) {
+            $scope.role = role;
+        };
 
-    $scope.addTicket = function() {
-        $scope.retroListElements.push ({
-            'role': $scope.noteType,
-            'message': $scope.retroMessage
-        });
-        $scope.retroMessage = '';
-    };
+        $scope.addTicket = function() {
+            $scope.retroTickets.push({
+                'role'   : $scope.role,
+                'message': $scope.retroMessage,
+                'retroId': $scope.retroId
+            });
+
+            // clear the message input box
+            $scope.retroMessage = '';
+        };
 
 
 
-    $scope.retroPublish = function () {
-        var params = {'retroId': $scope.retroId};
-        tickets.data = $scope.retroListElements;
-        tickets.save(params);
-    };
+        $scope.retroPublish = function () {
+            tickets.save($scope.retroTickets, function (response) {
+                console.log('did something');
+                console.log(response);
+            });
+        };
 });
