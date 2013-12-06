52bb595141a6d56496833dbac08cad8b5c1c6a9d
Gavin King <gavin.king@fronter.com>
2013-12-06 11:03:27 +0100

    Added polling to the board

--- static/app/scripts/controllers/board.js
+++ static/app/scripts/controllers/board.js
@@ -4,14 +4,24 @@ angular.module('retrospectApp')
     .controller('BoardCtrl', [
         '$scope',
         '$routeParams',
+        '$timeout',
         'tickets',
 
-        function ($scope, $routeParams, tickets) {
+        function ($scope, $routeParams, $timeout, tickets, BoardService) {
+            var updateTickets;
+
             $scope.retroId = $routeParams.retroId;
             $scope.tickets = [];
 
-            tickets.query({'retroId': $scope.retroId}, function (tickets) {
-                $scope.tickets = tickets;
-            });
+            updateTickets = function () {
+                console.log('Getting tickets');
+                tickets.query({'retroId': $scope.retroId}, function (tickets) {
+                    $scope.tickets = tickets;
+                    $timeout(updateTickets, 2000);
+                });
+            };
+
+            updateTickets();
         }
-    ]);
+    ]
+);
