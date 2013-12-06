a38767a781c9ea8579f99ab14751a8216893bed6
Gavin King <gavin.king@fronter.com>
2013-12-06 14:17:46 +0100

    Styling the ticket add board

--- static/app/scripts/controllers/editpage.js
+++ static/app/scripts/controllers/editpage.js
@@ -4,13 +4,27 @@ angular.module('retrospectApp')
 
     .controller('EditpageCtrl', function ($scope, $location, $routeParams, tickets) {
         $scope.tickets = [];
-        $scope.role = 'pro';
+        $scope.role;
 
         $scope.retroId = $routeParams.retroId;
 
         $scope.setRole = function (role) {
+            switch (role) {
+                case 'pro':
+                    $scope.placeholder = 'What went well?';
+                    break;
+                case 'con':
+                    $scope.placeholder = 'What did not go well?';
+                    break;
+                case 'puzzle':
+                    $scope.placeholder = 'What was confusing?';
+                    break;
+            }
+
             $scope.role = role;
         };
+
+        $scope.setRole('pro');
 
         $scope.createdAt = JSON.stringify(new Date());
 
