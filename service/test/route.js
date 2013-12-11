359255b3fe70cdb0df0c5395a1a1f85d1fcba94f
Petter Aas <petter.aas@fronter.com>
2013-12-11 22:21:56 +0100

    Started with unit-testing of /service/

---
+++ service/test/route.js
@@ -0,0 +1,79 @@
+var should = require('should'),
+    assert = require('assert'),
+    request = require('supertest'),
+//    winston = require('winston'),
+    sage = require('sage');
+    //config = require('./config-debug');
+
+describe('Routing', function () {
+    var url = 'http://localhost:3000',
+        es = null;
+
+    before(function(done) {
+        es = sage('http://localhost:9200');
+        done();
+    });
+
+    describe('Retrospectives GET all', function() {
+        it('Should return correct headers', function(done) {
+            request(url)
+                .get('/retrospectives')
+                .end(function(err, res) {
+                    checkHeaders(err, res, done);
+                });
+        });
+    });
+
+    /** Gives a
+     *      Cannot set property id of undefined at line 7 in wrapper.js
+     *
+    describe('Retrospectives GET', function() {
+        it('Should return correct headers', function(done) {
+            request(url)
+                .get('/retrospectives/sdf23423sdf')
+                .end(function(err, res) {
+                    checkHeaders(err, res, done);
+                });
+        });
+    });
+    */
+
+    describe('Retrospectives POST', function() {
+        it('Should return correct headers', function(done) {
+            request(url)
+                .post('/retrospectives')
+                .end(function(err, res) {
+                    checkHeaders(err, res, done);
+                });
+        });
+    });
+});
+
+
+var checkHeaders = function(err, res, done) {
+    if (err) {
+        throw err;
+    }
+    res.should.have.status(200);
+    res.should.have.header(
+            'Content-Type',
+            'application/json; charset=utf-8'
+            );
+
+    res.should.have.header(
+            'Access-Control-Allow-Methods',
+            'GET,PUT,POST,DELETE,OPTIONS'
+            );
+
+    res.should.have.header(
+            'Access-Control-Allow-Headers',
+            'X-Requested-With, Content-Type'
+            );
+
+    res.should.have.header(
+            'Access-Control-Allow-Origin',
+            '*'
+            );
+
+    done();
+};
