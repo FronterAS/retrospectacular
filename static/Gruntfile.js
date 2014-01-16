bfb6787c826c5183a62898bfbb24642ab9cdf3f2
Petter Aas <aaspet@gmail.com>
2014-01-17 00:48:06 +0100

    Make sure grunt builds views/directives/fuiEditable.html

--- static/Gruntfile.js
+++ static/Gruntfile.js
@@ -224,7 +224,7 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= yeoman.app %>',
-                    src: ['*.html', 'views/*.html'],
+                    src: ['*.html', 'views/*.html', 'views/*/*.html'],
                     dest: '<%= yeoman.dist %>'
                 }]
             }
