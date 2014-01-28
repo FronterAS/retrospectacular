6bc9d90ea6f3f648ed343f69d8bfc9b8535d034d
Kent Fjellstad <kent@fjellstad.name>
2014-01-28 14:35:47 +0100

    Changed fuiEditable so you don't need the callbacks for save and update. But then you can't save or update eighter

--- static/app/scripts/directives/fuiEditable.js
+++ static/app/scripts/directives/fuiEditable.js
@@ -10,17 +10,33 @@ angular.module('retrospectApp')
             item: '=',
             onDelete: '&',
             onUpdate: '&'
-            },
+        },
         transclude: true,
         link: function postLink(scope, element, attrs) {
             scope.toggleEditmode = function() {
                 scope.showEdit = !scope.showEdit;
-            }
+            };
+
+            scope.hasDelete = function() {
+                if (!attrs.onDelete) {
+                    return false;
+                } else {
+                    return true;
+                }
+            };
+
+            scope.hasUpdate = function() {
+                if (!attrs.onUpdate) {
+                    return false;
+                } else {
+                    return true;
+                }
+            };
 
             scope.saveChange = function () {
                 scope.onUpdate();
                 scope.toggleEditmode();
-            }
+            };
         }
     };
     });
