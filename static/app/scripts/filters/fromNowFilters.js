95353aaeec118fde3377358e4e4439ec9e504ae5
Petter Aas <petter.aas@fronter.com>
2014-02-06 15:45:39 +0100

    Fixes jshint complaints

--- static/app/scripts/filters/fromNowFilters.js
+++ static/app/scripts/filters/fromNowFilters.js
@@ -1,6 +1,8 @@
+/*global moment*/
 angular.module('retrospectApp')
     .filter('fromNow', function () {
+        'use strict';
         return function (dateString) {
-            return moment(dateString).fromNow()
+            return moment(dateString).fromNow();
         };
     });
