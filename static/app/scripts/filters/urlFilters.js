2ebaef3b5fb93a52c78d4c62d643a991e6e319c5
Petter Aas <aaspet@gmail.com>
2014-02-23 20:26:42 +0100

    Only replace urls in existing text.

--- static/app/scripts/filters/urlFilters.js
+++ static/app/scripts/filters/urlFilters.js
@@ -12,15 +12,17 @@ angular.module('retrospectApp')
             eMailReplacePattern = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
 
         return function(text) {
-            angular.forEach(text.match(protocolReplacePattern), function() {
-                text = text.replace(protocolReplacePattern, '<a href="$1" target="_blank">$1</a>');
-            });
-            angular.forEach(text.match(w3ReplacePattern), function() {
-                text = text.replace(w3ReplacePattern, '$1<a href="http://$2" target="_blank">$2</a>');
-            });
-            angular.forEach(text.match(eMailReplacePattern), function() {
-                text = text.replace(eMailReplacePattern, '<a href="mailto:$1">$1</a>');
-            });
+            if (text) {
+                angular.forEach(text.match(protocolReplacePattern), function() {
+                    text = text.replace(protocolReplacePattern, '<a href="$1" target="_blank">$1</a>');
+                });
+                angular.forEach(text.match(w3ReplacePattern), function() {
+                    text = text.replace(w3ReplacePattern, '$1<a href="http://$2" target="_blank">$2</a>');
+                });
+                angular.forEach(text.match(eMailReplacePattern), function() {
+                    text = text.replace(eMailReplacePattern, '<a href="mailto:$1">$1</a>');
+                });
+            }
 
             return text;
         };
