6921205519fdcdc58107881767b36fa7e18d3148
Petter Aas <aaspet@gmail.com>
2014-02-23 20:18:42 +0100

    Made ngSanitize available in karma

--- static/karma.conf.js
+++ static/karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular-sanitize/angular-sanitize.js',
       'app/bower_components/momentjs/moment.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
