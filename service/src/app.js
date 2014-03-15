38daf2098c9519dda226092cbf9f7a0da5c4768d
Petter Aas <aaspet@gmail.com>
2014-03-15 09:05:19 +0100

    Handle missing newrelic config.

--- service/src/app.js
+++ service/src/app.js
@@ -1,7 +1,11 @@
 try {
     require('newrelic');
 } catch (e) {
-    console.log('Optional module "newrelic" was not installed');
+    if ( e.code === 'MODULE_NOT_FOUND') {
+        console.log('Run npm install without the --no-optional flag to install "newrelic"!');
+    } else {
+        console.log('Copy service/newrelic.js.example to service/newrelic.js');
+    }
 }
 
 var routes = require('./routes'),
