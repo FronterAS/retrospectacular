5fe81ef7b87719e50cba817d4f3c042f49e58035
Gavin King <gavin.king@pearson.com>
2015-03-30 11:41:18 +0200

    Removed new relic

--- service/src/app.js
+++ service/src/app.js
@@ -1,14 +1,4 @@
 'use strict';
-try {
-    require('newrelic');
-} catch (e) {
-    if ( e.code === 'MODULE_NOT_FOUND') {
-        console.log('Run npm install without the --no-optional flag to install "newrelic"!');
-    } else {
-        console.log('Copy service/newrelic.js.example to service/newrelic.js');
-    }
-}
-
 var routes = require('./routes'),
     logs = require('./logs'),
     express = require('express'),
